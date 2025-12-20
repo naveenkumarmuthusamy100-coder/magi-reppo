c3aa7f12fbedd265659cb2f2b71d540917885260
naveenkumarmuthusamy100-coder <naveenkumarmuthusamy@gmail.com>
2025-12-20 11:13:16 +0530

    firstcommit

---
+++ FoodIndian.java
@@ -0,0 +1,17 @@
+class FoodIndian
+{
+	public static void main(String[] args)
+	{
+		String foodname= "Paneer Butter Masala";
+		String cusinetype= "Indian";
+		int calories= 400;
+		boolean vegetarian= true;
+		double price= 250.50;
+		
+			System.out.println(foodname);
+			System.out.println(cusinetype);
+			System.out.println(calories);
+			System.out.println(vegetarian);
+			System.out.println(price);
+	}
+}
