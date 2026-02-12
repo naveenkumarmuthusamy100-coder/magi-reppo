deadefbe2c6eead0df823a2380549b4af2816c86
naveenkumarmuthusamy100-coder <naveenkumarmuthusamy@gmail.com>
2026-02-12 07:02:59 +0530

    Day 11 Homework

---
+++ ReverseCounting.java
@@ -0,0 +1,14 @@
+class ReverseCounting
+{
+    public static void main(String[] args)
+    {
+        int n = 5;
+
+        System.out.println("Output Reverse Numbers:");
+
+        for(int i = n; i >= 1; i--)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ Table.java
@@ -0,0 +1,13 @@
+class Table 
+{
+    public static void main(String[] args)
+	{
+        
+        int number = 7;
+
+        for(int i = 1; i <= 10; i++) 
+		{
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
